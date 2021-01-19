858c9e0274d947dfe073aea130c9bf87512a9891
TPD <mig.mic@gmail.com>
2021-01-19 15:02:37 +0100

    Initial commit

---
+++ mpDat19c/src/main/java/com/mp/demo/Repository/SupervisorRepository.java
@@ -0,0 +1,10 @@
+package com.mp.demo.Repository;
+
+import com.mp.demo.model.Supervisor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SupervisorRepository extends CrudRepository<Supervisor,Long> {
+
+
+
+}
---
+++ mpDat19c/src/main/java/com/mp/demo/controller/StudentRestController.java
@@ -0,0 +1,67 @@
+package com.mp.demo.controller;
+
+import com.mp.demo.Repository.StudentRepository;
+import com.mp.demo.Repository.SupervisorRepository;
+import com.mp.demo.model.Student;
+import com.mp.demo.model.Supervisor;
+import com.mp.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class StudentRestController {
+
+    @Autowired
+    StudentRepository studentRepository;
+   // SupervisorRepository supervisorRepository;
+
+    @GetMapping("/student")
+    public Iterable<Student> findall() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Optional<Student>> findbyid(@PathVariable Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        if (student.isPresent()) {
+            return ResponseEntity.status(200).body(student);
+        } else {
+            return ResponseEntity.status(404).body(student);
+        }
+    }
+
+    @CrossOrigin(origins = "*", exposedHeaders = "StudentID")
+    @PostMapping(value = "/student", consumes = {"application/json"})
+    public ResponseEntity<String> create(@RequestBody Student st) {
+        Student _st = new Student(st.getName(), st.getEmail());
+        _st.setSupervisor(st.getSupervisor());
+        studentRepository.save(_st);
+        return ResponseEntity.status(201).header("StudentID", "/student/" + _st.getId()).body("{'msg': 'Post created'}");
+    }
+
+    @CrossOrigin(origins = "*", exposedHeaders = "Location")
+    @PutMapping(value = "/student", consumes = {"application/json"})
+    public ResponseEntity<String> update(@RequestBody Student st) {
+        Optional<Student> optionalStudent = studentRepository.findById(st.getId());
+        if (optionalStudent.isEmpty()) {
+            return ResponseEntity.status(404).body("{'msg'} :Not found ");
+        }
+        studentRepository.save(st);
+        return ResponseEntity.status(200).body("{'msg'}: Update");
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        Optional<Student> stud = studentRepository.findById(id);
+        if (stud.isEmpty()) {
+            return ResponseEntity.status(404).body("{'msg'; Not found!}");
+        }
+        studentRepository.deleteById(id);
+        return ResponseEntity.status(200).body("{'msg'}: Deleted");
+    }
+
+
+}
